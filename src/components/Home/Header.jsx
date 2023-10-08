cfb4899dd9ccefc5a90a88b7734fec5fa649a7aa
femmyte <sanyaoluadefemi@gmail.com>
2023-10-08 01:02:12 +0100

    Adjusted the header component for mobile responsiveness

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -3,13 +3,15 @@ import ButtonLink from '../common/ButtonLink';
 
 const Header = ({ children }) => {
 	return (
-		<header className='text-center px-4 md:px-[4rem] py-[3.12rem] bg-black h-screen md:h-[83vh] '>
-			<h1 className='text-naturalWhite font-[600] text-[1.5rem] md:text-[4rem] font-oswald leading-normal'>
-				WE ARE A UIUX DESIGNER, GRAPHICS DESIGN{' '}
-				<span className=' text-primaryYellow'>
-					& IDENTITY DESIGN AGENCY
-				</span>
-			</h1>
+		<header className='text-center px-4 md:px-[4rem]  py-[3.12rem] bg-black h-screen md:h-[83vh] flex flex-col justify-center md:justify-start '>
+			<div className='md:w-[72.75rem]'>
+				<h1 className='text-naturalWhite font-[600] text-[1.8rem] md:text-[4rem] font-oswald leading-normal'>
+					WE ARE A UIUX DESIGNER, GRAPHICS DESIGN{' '}
+					<span className=' text-primaryYellow'>
+						& IDENTITY DESIGN AGENCY
+					</span>
+				</h1>
+			</div>
 			<div className='mt-[3.38rem]'>
 				<ButtonLink text={'VIEW WORK'} link={'#'} />
 			</div>
