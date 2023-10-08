fbc8ae6ff96827d1ae16a38daffb011b0e0d1ead
femmyte <sanyaoluadefemi@gmail.com>
2023-10-08 01:04:01 +0100

    Adjusted the header component for mobile responsiveness

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -3,7 +3,7 @@ import ButtonLink from '../common/ButtonLink';
 
 const Header = ({ children }) => {
 	return (
-		<header className='text-center px-4 md:px-[4rem]  py-[3.12rem] bg-black h-screen md:h-[83vh] flex flex-col justify-center md:justify-start '>
+		<header className='text-center px-4 md:px-[4rem]  py-[3.12rem] bg-black md:h-[83vh] flex flex-col justify-center md:justify-start '>
 			<div className='md:w-[72.75rem]'>
 				<h1 className='text-naturalWhite font-[600] text-[1.8rem] md:text-[4rem] font-oswald leading-normal'>
 					WE ARE A UIUX DESIGNER, GRAPHICS DESIGN{' '}
